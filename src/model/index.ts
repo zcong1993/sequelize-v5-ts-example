55b123049d94dd0cc8aa2ec10db2a1588541d069
zcong1993 <zhangcong1992@gmail.com>
2019-11-18 13:58:23 +0800

    add commit

--- src/model/index.ts
+++ src/model/index.ts
@@ -4,14 +4,17 @@ import { Tag } from './tag'
 import { TagPost } from './tagPost'
 import { UserDetail } from './userDetails'
 
+// 1:n
 User.hasMany(Post, { constraints: false, as: 'posts', foreignKey: 'authorId' })
 Post.belongsTo(User, {
   constraints: false,
   as: 'author',
   foreignKey: 'authorId'
 })
+// n:m
 Post.belongsToMany(Tag, { constraints: false, through: TagPost })
 Tag.belongsToMany(Post, { constraints: false, through: TagPost })
+// 1:1
 User.hasOne(UserDetail, {
   constraints: false,
   as: 'detail',
